ae92f52c22142f8ae27a140304484959a2ed48be
Julio Gaekel <jriveragaekel@gmail.com>
2024-02-20 13:03:45 +0100

    Include functions.php
    - Deleted code was used to create a function called updateTable and included from the functions.php file for cleaner code and better readability.

--- mysql/loginUpdate.php
+++ mysql/loginUpdate.php
@@ -1,38 +1,24 @@
+<?php global $connection, $result;
+include "database.php";
+include "functions.php";
+
+// Use include function to use a file from different source. In this case, we use database.php to connect to db making code clearer and modular.
+?>
+
 <?php
 
-    // Connect to database
-    $connection = mysqli_connect('localhost', 'root', '', 'testLoginApp');
-
-    // Test database connection
-    if ($connection) {
-        echo "STATUS: You are connected to the database";
-        echo "<br>";
-        echo "<br>";
-        echo "<br>";
-        echo "<br>";
-    } else {
-        die("STATUS: Database connection failed");
-    }
-
-    // Read data from database
-    $query = "SELECT * FROM users";
-    $result = mysqli_query($connection, $query);
-
-    // Check on the query
-    if (!$result) {
-        die("Query FAILED" . mysqli_error());
-    }
+updateTable();
 
 ?>
 
 
 <!doctype html>
 <html lang="en">
-<head>fgnfgfggf
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Update</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
@@ -41,20 +27,32 @@
 
     <div class="col-sm-6">
 
-        // Display data. This code displays data organized and grouped by user
-        <?php
-            while ($row = mysqli_fetch_assoc($result)) {
-        ?>
 
-        <pre>
-            <?php
-                print_r($row);
-            ?>
-        </pre>
+        <form action="loginUpdate.php" method="post">
+            <br>
+            <div class="form-group">
+                <select name="id" id="">
 
-        <?php
-        }
-        ?>
+                    <?php
+                        showAllData();
+                    ?>
+
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <br>
+            <input class="btn btn-primary" type="submit" name="submit" value="UPDATE">
+        </form>
+
 
     </div>
 
