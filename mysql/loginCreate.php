9cb015ff0277445525cecfe1d07676b50f3b7874
Julio Gaekel <jriveragaekel@gmail.com>
2024-02-20 13:00:08 +0100

    Delete database connection
    Connection was deleted from this file and included from database.php for cleaner code. Original code was saved as comment for future references.

--- mysql/loginCreate.php
+++ mysql/loginCreate.php
@@ -1,20 +1,10 @@
-<?php
+<?php include "database.php";
 // Pick up the data
 if (isset($_POST['submit'])) { // If submitted
 
     // Store incoming data in variables
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    // Connect to database
-    $connection = mysqli_connect('localhost', 'root', '', 'testLoginApp');
-
-    // Test database connection
-    if ($connection) {
-        echo "You are connected to the database";
-    } else {
-        die("Database connection failed");
-    }
 
     // Insert data into database
     $query = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
@@ -26,17 +16,26 @@ if (isset($_POST['submit'])) { // If submitted
     }
 
 
-
-    /*    // Check if username and password are entered.
-        if ($username && $password) {
-            echo $username;
-            echo $password;
-        } else {
-            echo "This cannot be blank";
-        }*/
+/*    // Check if username and password are entered.
+    if ($username && $password) {
+        echo $username;
+        echo $password;
+    } else {
+        echo "This cannot be blank";
+    }*/
 
 }
 
+/* This code block has been copied to the database.php
+    // Connect to database
+    $connection = mysqli_connect('localhost', 'root', '', 'testLoginApp');
+
+    // Test database connection
+    if ($connection) {
+        echo "You are connected to the database";
+    } else {
+        die("Database connection failed");
+    }*/
 ?>
 
 
@@ -46,7 +45,7 @@ if (isset($_POST['submit'])) { // If submitted
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Create</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
