0b41a7737c3a20c289d15265dd206d5129179b6e
Julio Gaekel <jriveragaekel@gmail.com>
2024-02-20 15:50:10 +0100

    Refactor all files modular
    - Making all files modular and refactoring

--- mysql/loginCreate.php
+++ mysql/loginCreate.php
@@ -1,33 +1,12 @@
-<?php global $connection;
-include "database.php";
-include "functions.php"
-/* This code block has been copied to the database.php
-    // Connect to database
-    $connection = mysqli_connect('localhost', 'root', '', 'testLoginApp');
-
-    // Test database connection
-    if ($connection) {
-        echo "You are connected to the database";
-    } else {
-        die("Database connection failed");
-    }*/
-?>
-<?php
-createRows();
-?>
+<?php global $connection;?>
+<?php include "database.php";?>
+<?php include "functions.php";?>
 
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</head>
-<body>
+<?php createRows();?>
+
+<?php include "Includes/header.php";?>
+
 <div class="container">
 <h1 class="text-center">Create</h1>
     <div class="col-sm-6">
@@ -46,7 +25,5 @@ createRows();
         </form>
     </div>
 
-</div>
-</body>
-</html>
+<?php include "Includes/footer.php";?>
 
--- mysql/loginUpdate.php
+++ mysql/loginUpdate.php
@@ -1,28 +1,13 @@
-<?php global $connection, $result;
-include "database.php";
-include "functions.php";
+<?php global $connection, $result;?>
+<?php include "database.php";?>
+<?php include "functions.php";?>
 
-// Use include function to use a file from different source. In this case, we use database.php to connect to db making code clearer and modular.
-?>
+<!--// Use include function to use a file from different source. In this case, we use database.php to connect to db making code clearer and modular.-->
 
-<?php
+<?php updateTable();?>
 
-updateTable();
+<?php include "Includes/header.php";?>
 
-?>
-
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Update</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</head>
-<body>
 <div class="container">
 <h1 class="text-center">Update</h1>
     <div class="col-sm-6">
@@ -56,7 +41,5 @@ updateTable();
 
     </div>
 
-</div>
-</body>
-</html>
+<?php include "Includes/footer.php";?>
 
