d1a8a0977ab28e87fa24061b09d7172ee29e59c1
Julio Gaekel <jriveragaekel@gmail.com>
2024-02-21 14:40:38 +0100

    add ids to form inputs

--- mysql/loginCreate.php
+++ mysql/loginCreate.php
@@ -13,12 +13,12 @@
         <form action="loginCreate.php" method="post">
             <div class="form-group">
                 <label for="username">Username</label>
-                <input type="text" name="username" class="form-control">
+                <input type="text" name="username" id="username" class="form-control">
             </div>
 
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" name="password" class="form-control">
+                <input type="password" name="password" id="password" class="form-control">
             </div>
             <br>
             <input class="btn btn-primary" type="submit" name="submit" value="Create">
