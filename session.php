f6cd65b473701d4a5552ce1b8f4592b74b658d67
Julio Gaekel <jriveragaekel@gmail.com>
2024-02-22 09:25:56 +0100

    Create practice files
    - These files are only for practice purposes and were created watching the video tutorials. Saved for future references.

---
+++ session.php
@@ -0,0 +1,20 @@
+<?php
+session_start(); // NEeds to be on every page that is intented to be used at
+
+$_SESSION['greeting'] = "Hello Student, this is awesome";
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
